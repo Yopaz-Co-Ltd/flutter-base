dff3f7938c62ef77af9620312b5272c965209373
thiendt3 <108515038+thiendt3@users.noreply.github.com>
2023-04-14 10:22:46 +0700

    Create widget image helper (#1)
    * create widget image helper
    
    * refactor code create image helper
    
    * fix comment

---
+++ lib/assets/images/Images.dart
@@ -0,0 +1,4 @@
+class Images {
+  static const String iconLogo = 'lib/assets/images/icon_logo.svg';
+  static const String iconUser = 'lib/assets/images/icon_user.png';
+}
--- lib/screens/home/home_screen_state.dart
+++ lib/screens/home/home_screen_state.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_base/assets/images/Images.dart';
 import 'package:flutter_base/assets/translations/localization.dart';
 import 'package:flutter_base/repositories/authentication_reposotory/bloc/authentication_bloc.dart';
 import 'package:flutter_base/repositories/authentication_reposotory/bloc/authentication_event.dart';
 import 'package:flutter_base/screens/home/home_screen.dart';
 import 'package:flutter_base/widgets/button/button.dart';
+import 'package:flutter_base/widgets/image/image.dart';
+import 'package:flutter_base/widgets/image/svg_image.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 
@@ -27,6 +30,22 @@ class HomeScreenState extends State<HomeScreen> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ImageWidget(
+                    imageFilePath: Images.iconUser,
+                    width: 40,
+                    height: 40,
+                  ),
+                  SvgImageWidget(
+                    imageFilePath: Images.iconLogo,
+                    radius: BorderRadius.all(Radius.circular(50)),
+                    width: 50,
+                    height: 50,
+                  ),
+                ],
+              ),
               Center(
                 child: Text(
                   username ?? "",
